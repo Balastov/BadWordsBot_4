9ec46d58bd7a2b17f8642d4a83f9ff2533014aef
Balastov <m0000nspe1111@gmail.com>
2026-02-26 16:13:26 +0300

    Вернули кнопку Мемы

--- BadWords_4.py
+++ BadWords_4.py
@@ -633,7 +633,7 @@ def back_from_admin(message):
 
 @bot.message_handler(func=lambda message: message.text == 'Мемы')
 def send_meme_from_menu(message):
-    """Отправляет случайный мем при нажатии кнопки"""
+    """Отправляет случайный мем при нажатии кнопки!"""
     bot.send_chat_action(message.chat.id, 'upload_photo')
     meme_sender.send_meme_now()
 
